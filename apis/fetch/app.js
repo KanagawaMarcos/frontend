6ed553adef5dcc4a5b3b331c3af7aaa0af02c520
Marcos Costa <mcs1901@outlook.com>
2019-06-10 09:55:32 -0300

    make a function that fetchs a file and extract its text

--- apis/fetch/app.js
+++ apis/fetch/app.js
@@ -1,9 +1,19 @@
+// multiple images
+
 catchRainbow().catch((error)=>{
     console.error("some error has happened:")
     console.error(error)
 });
+
 async function catchRainbow(){
     const response = await fetch('rainbow.png');
     const blob = await response.blob();
     document.getElementById('rainbow').src = URL.createObjectURL(blob);
+}
+
+async function catchLorem (){
+
+    const response = await fetch('lorem.md');
+    const text = await response.text();
+    document.querySelector('#lorem').innerHTML = text;
 }
