0d3ec54936e306808c5604e43d298f68daec56bb
Marcos Costa <mcs1901@outlook.com>
2019-06-10 10:28:20 -0300

    create the catch multiple images function, but it doesnt work

--- apis/fetch/app.js
+++ apis/fetch/app.js
@@ -1,19 +1,28 @@
-// multiple images
+// Functions 
 
-catchRainbow().catch((error)=>{
-    console.error("some error has happened:")
-    console.error(error)
-});
-
-async function catchRainbow(){
-    const response = await fetch('rainbow.png');
-    const blob = await response.blob();
-    document.getElementById('rainbow').src = URL.createObjectURL(blob);
+const catchImage = async (filename) => {
+    const response = await fetch(filename);
+    return blob = await response.blob();
 }
 
-async function catchLorem (){
-
-    const response = await fetch('lorem.md');
-    const text = await response.text();
-    document.querySelector('#lorem').innerHTML = text;
+const catchImages = async (filesname) => {
+    filesname.map(filename => catchImage(filename))
 }
+
+const catchText = async (filename) => {
+    const response = await fetch(filename);
+    return text = await response.text();
+}
+
+const image = catchImage('rainbow.png').catch(error => console.error(error));
+document.getElementById('rainbow').src = URL.createObjectURL(image);
+
+const text = catchText('lorem.md').catch(error => console.error(error));
+document.querySelector('#lorem').innerHTML = text;
+
+const filesname = ['cat.png','rainbow.png','dog.png'];
+
+const promises = catchImages(filesname);
+const images = Promise.all(promises);
+console.log(images);
+//const img_elements = images
